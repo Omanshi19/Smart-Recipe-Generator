678dfc463dcb891c5b8748780575e51b1874e837
Omanshi Kaushal <132755038+Omanshi19@users.noreply.github.com>
2026-06-21 20:57:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Main from "./Components/Main";
+import Main from "./Components/Main"; 
 import {Provider} from "react-redux"
 import { store } from "./Redux/store";
 function App() {
